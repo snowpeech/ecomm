531d2a9d04452d1dbb0c38d03ecee98c05d2b437
Lydia <snowpeech@gmail.com>
2020-01-14 00:59:16 -0600

    finished styling homepage

--- src/homepage.component.js
+++ src/homepage.component.js
@@ -1,40 +1,11 @@
 import React from "react";
+import Directory from "./directory.component";
+
 import "./homepage.styles.scss";
 
 const HomePage = () => (
   <div className='homepage'>
-    <div className='directory-menu'>
-      <div className='menu-item'>
-        <div className='content'>
-          <h1>Hats</h1>
-          <span>Shop Now</span>
-        </div>
-      </div>
-      <div className='menu-item'>
-        <div className='content'>
-          <h1>Jackets</h1>
-          <span>Shop Now</span>
-        </div>
-      </div>
-      <div className='menu-item'>
-        <div className='content'>
-          <h1>Sweaters</h1>
-          <span>Shop Now</span>
-        </div>
-      </div>
-      <div className='menu-item'>
-        <div className='content'>
-          <h1>Women</h1>
-          <span>Shop Now</span>
-        </div>
-      </div>
-      <div className='menu-item'>
-        <div className='content'>
-          <h1>Men</h1>
-          <span>Shop Now</span>
-        </div>
-      </div>
-    </div>
+    <Directory />
   </div>
 );
 
