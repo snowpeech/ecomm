05fe18514988e2848a79b5932bdac56b191fb178
Lydia <snowpeech@gmail.com>
2020-01-22 11:38:08 -0600

    added a sign out button for logged in users and added styling to google sign in button

--- src/components/header.component.jsx
+++ src/components/header.component.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 import { ReactComponent as Logo } from "../assets/crown.svg";
+import { auth } from "../firebase/firebase.utils";
 
 import "../styles/header.styles.scss";
 
-const Header = () => (
+const Header = ({ currentUser }) => (
   <div className='header'>
     <Link to='/'>
       <Logo className='logo' />
@@ -19,6 +20,15 @@ const Header = () => (
         {" "}
         contact?
       </Link>
+      {currentUser ? (
+        <div className='option' onClick={() => auth.signOut()}>
+          Sign Out
+        </div>
+      ) : (
+        <Link className='option' to='/signIn'>
+          Sign In
+        </Link>
+      )}
     </div>
   </div>
 );
