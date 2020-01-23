a077743d7da7c4ac6618392262cfdc42e5607b85
Lydia <snowpeech@gmail.com>
2020-01-23 14:40:43 -0600

    updated sign in component so user can use email to sign in

--- src/components/sign-in.component.jsx
+++ src/components/sign-in.component.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import FormInput from "./form-input.component";
 import CustomButton from "./custom-button.component";
 
-import { signInWithGoogle } from "../firebase/firebase.utils";
+import { auth, signInWithGoogle } from "../firebase/firebase.utils";
 
 import "../styles/sign-in.styles.scss";
 
@@ -16,10 +16,16 @@ class SignIn extends React.Component {
     };
   }
 
-  handleSubmit = event => {
+  handleSubmit = async event => {
     event.preventDefault();
 
-    this.setState({ email: "", passord: "" });
+    const { email, password } = this.state;
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({ email: "", passord: "" });
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   handleChange = event => {
