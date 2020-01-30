83a55a673cc9f012e84624a654dae6027ff6e951
Lydia <snowpeech@gmail.com>
2020-01-30 16:42:10 -0600

    items are added to cart state

--- src/components/merch-item.component.jsx
+++ src/components/merch-item.component.jsx
@@ -1,20 +1,35 @@
 import React from "react";
+import { connect } from "react-redux";
+
+import CustomButton from "./custom-button.component";
+import { addItem } from "../redux/cart/cart.actions";
 
 import "../styles/merch-item.styles.scss";
 
-const MerchItem = ({ id, name, price, imageUrl }) => (
-  <div className='merch-item'>
-    <div
-      className='image'
-      style={{
-        backgroundImage: `url(${imageUrl})`
-      }}
-    />
-    <div className='item-footer'>
-      <span className='name'>{name}</span>
-      <span className='price'>{price}</span>
-    </div>
-  </div>
-);
+const MerchItem = ({ item, addItem }) => {
+  const { name, price, imageUrl } = item;
 
-export default MerchItem;
+  return (
+    <div className='merch-item'>
+      <div
+        className='image'
+        style={{
+          backgroundImage: `url(${imageUrl})`
+        }}
+      />
+      <div className='item-footer'>
+        <span className='name'>{name}</span>
+        <span className='price'>{price}</span>
+      </div>
+      <CustomButton onClick={() => addItem(item)} inverted>
+        Add to cart
+      </CustomButton>
+    </div>
+  );
+};
+
+const mapDispatchToProps = dispatch => ({
+  addItem: item => dispatch(addItem(item))
+});
+
+export default connect(null, mapDispatchToProps)(MerchItem);
