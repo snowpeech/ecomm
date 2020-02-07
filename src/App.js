df4e3488c5d15f80abb8ed46bb8841768a572216
Lydia <snowpeech@gmail.com>
2020-02-07 17:49:26 -0600

    added checkout page, checkout item component, and updated cart to use selectors

--- src/App.js
+++ src/App.js
@@ -6,13 +6,13 @@ import { createStructuredSelector } from "reselect";
 import { setCurrentUser } from "./redux/user/user.actions";
 
 import "./App.css";
-
-import HomePage from "./pages/homepage.component";
 import ShopPage from "./pages/shop.components";
 import Header from "./components/header.component";
 import SignInRegisterPage from "./pages/sign-in-register.component";
+import HomePage from "./pages/homepage.component";
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 import { selectCurrentUser } from "./redux/user/user.selector";
+import CheckoutPage from "./pages/checkout.component";
 
 class App extends React.Component {
   unsubscribeFromAuth = null;
@@ -44,6 +44,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
+          <Route exact path='/checkout' component={CheckoutPage} />
           <Route
             exact
             path='/signin'
--- src/components/cart-dropdown.component.jsx
+++ src/components/cart-dropdown.component.jsx
@@ -1,25 +1,43 @@
 import React from "react";
 import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
+import { createStructuredSelector } from "reselect";
 
 import CustomButton from "./custom-button.component";
 import CartItem from "./cart-item.component";
 import { selectCartItems } from "../redux/cart/cart.selector";
+import { toggleCartHidden } from "../redux/cart/cart.actions";
+
 import "../styles/cart-dropdown.styles.scss";
 
-const CartDropdown = ({ cartItems }) => (
-  <div className='cart-dropdown'>
-    <div className='cart-items'>
-      {cartItems.map(cartItem => (
-        <CartItem key={cartItem.id} item={cartItem} />
-      ))}
+const CartDropdown = ({ cartItems, history, dispatch }) => {
+  return (
+    <div className='cart-dropdown'>
+      <div className='cart-items'>
+        {cartItems.length ? (
+          cartItems.map(cartItem => (
+            <CartItem key={cartItem.id} item={cartItem} />
+          ))
+        ) : (
+          <span className='empty-message'>Your cart is empty</span>
+        )}
+      </div>
+      <CustomButton
+        onClick={() => {
+          history.push("/checkout");
+          dispatch(toggleCartHidden());
+        }}
+      >
+        Go to Checkout
+      </CustomButton>
     </div>
-    <CustomButton>Go to Checkout</CustomButton>
-  </div>
-);
+  );
+};
 
-const mapStateToProps = state => ({
-  cartItems: selectCartItems
-});
+const mapStateToProps = state =>
+  createStructuredSelector({
+    cartItems: selectCartItems
+  });
 
 export default withRouter(connect(mapStateToProps)(CartDropdown));
+//when you wrap components like this, it evaluates inside out. this way our component will have access to the desired props
