6826e9156279c4a49e528cb78b28607d6f256ab8
Lydia <snowpeech@gmail.com>
2020-02-09 16:44:34 -0600

    added redux-persist and updated root reducer and indexjs to persist state

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,19 @@
 import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; //gives you local storage object on our browser
 
 import userReducer from "./user/user.reducer";
 import cartReducer from "./cart/cart.reducer";
 
-export default combineReducers({
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["cart"] //user is handled by firebase authentication. whitelist lets redux-persist know what to persist
+};
+
+const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer
-});
+})
+
+export default persistReducer(persistConfig, rootReducer);
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,14 @@
 import { createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
+import { persistStore } from "redux-persist";
 
 import rootReducer from "./root-reducer";
 
 //set up middleware. The store we're expecting is an array
 const middlewares = [logger];
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-export default store;
+export const persistor = persistStore(store);
+
+export default { store, persistor };
